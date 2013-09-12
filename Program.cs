6c43bbaa2c6fb22b9636ae0cebbec21aea8c0c4a
AlexeyBudko <lesha_11@bk.ru>
2013-09-13 00:05:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apples_and_trees
+{
+    class Tree
+    {
+        public Tree()
+        {
+            numOfApples = 0;
+        }
+
+        public Tree(int amountOaApples)
+        {
+            numOfApples = amountOaApples;
+        }
+
+        private int numOfApples
+        {
+            set;
+            get;
+        }
+
+        public int Grow()
+        {
+            Random rand = new Random();
+            int grownApples = rand.Next(100500);
+            numOfApples += grownApples;
+            return grownApples;
+        }
+    }
+
+    class Apple
+    {
+        int numOfPips
+        {
+            set;
+            get;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
